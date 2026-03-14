e4519da163166ed0ea509cb4c9054e535559db8b
Hugo Gago <hugogago28@gmail.com>
2026-03-14 17:04:34 +0100

    login

---
+++ booktrack/src/main/java/es/colegiocalasanz/booktrack/controller/HomeController.java
@@ -0,0 +1,20 @@
+package es.colegiocalasanz.booktrack.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("nombre", "Álvaro");
+        return "home";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
